0b9124e4d01bd54a4b0fac8aff25c0828a63e81e
hashemialii <alihashemi9675@gmail.com>
2022-08-31 11:36:47 +0430

    CRUD

--- check_db.py
+++ check_db.py
@@ -3,8 +3,11 @@ import sqlite3
 
 db = sqlite3.connect('employee.sqlite')
 
+# Retrieve Data:
 people_query = db.execute('SELECT * FROM people')
 id_name = db.execute('SELECT id, name FROM people')
+
+salary_query = db.execute('SELECT * FROM salary')
 
 print(people_query)
 
@@ -16,4 +19,8 @@ for item in people_query:
 for item in id_name:
     print(item)
 
+print('=' * 40)
+
+for item in salary_query:
+    print(item)
 
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sqlite3
+import datetime
 
 
 db = sqlite3.connect('employee.sqlite')
 
-# search ==> sqlite python crate table
+# search ==> sqlite python Create table
 
+# Create table:
 # create_query = 'CREATE TABLE IF NOT EXISTS people (' \
 #                'id INTEGER PRIMARY KEY, ' \
 #                'name TEXT NOT NULL, ' \
@@ -25,8 +27,22 @@ db.execute(create_query)
 # db.execute('INSERT INTO people (id, name, phone, department) '
 #            'VALUES(1001, "Ali Hashemi", "09121234567", "IT")')
 
-db.execute('INSERT INTO people VALUES(1002, "Peyman Haghighi", "09124569988", "SUPPORT")')
 
+# db.execute('INSERT INTO people VALUES(1002, "Peyman Haghighi", "09124569988", "SUPPORT")')
+
+today = datetime.date.today()
+# db.execute('INSERT INTO salary VALUES(1001, 15000, "{}")'.format(today))
+# db.execute('INSERT INTO salary VALUES(1002, 17500, "{}")'.format(today))
+# db.execute('INSERT INTO salary VALUES(1003, 18000, "{}")'.format(today))
+
+
+# Delete Data
+# db.execute('DELETE FROM salary WHERE id=1001')
+# db.execute('DELETE FROM salary WHERE id=1002')
+# db.execute('DELETE FROM salary WHERE id=1003')
+
+# update Data
+db.execute('UPDATE salary SET amount=25000 WHERE id=1002')
 
 # after do sth in your database u should commit and close your db
 db.commit()
